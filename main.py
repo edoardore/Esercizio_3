f98caa6e2dac00b88622ce18bd4e7e92011a0813
edoardore <edoardore@me.com>
2017-04-17 19:18:45 +0200

    1.5 final

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from TestInsertion import TestInsBackwards
 from TestMergeSort import TestMergeSortShuffle
 from TestMergeSort import TestMergeSortOrdered
 
+# test di insertion sort nel caso in cui i valori da ordinare sono casuali
 TestInsShuffle()
+# test di insertion sort nel caso in cui i valori da ordinare sono ordinati
 TestInsOrdered()
+# test di insertion sort nel caso in cui i valori da ordinare sono ordinati al contrario
 TestInsBackwards()
 
+# test di merge sort nel caso in cui i valori da ordinare sono casuali
 TestMergeSortShuffle()
+# test di merge sort nel caso in cui i valori da ordinare sono ordinati
 TestMergeSortOrdered()
-
-
